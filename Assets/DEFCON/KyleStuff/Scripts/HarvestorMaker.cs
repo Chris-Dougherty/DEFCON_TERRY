d8621fd914c3e9dcaba78c444dfa4bb4bad44d9f
Kmann180 <7319kkennedy@gmail.com>
2015-04-12 02:37:22 -0700

    Harvester
    Made Script, SHOULD work

---
+++ Assets/DEFCON/KyleStuff/Scripts/HarvestorMaker.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HarvestorMaker : MonoBehaviour 
+{
+	public GameObject HUnit;
+
+	public int HCost = 300;
+	
+	public GameObject IncomeMan;
+
+	// Use this for initialization
+	void Start () 
+	{
+		IncomeMan = GameObject.FindGameObjectWithTag("IncomeManager");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	public void SpawnHUnit()
+	{
+		if (IncomeMan.GetComponent<Income>().income >= 0 + HCost)
+		{
+			GameObject clone;
+			clone = Instantiate(HUnit, new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z - 1), Quaternion.identity) as GameObject;
+			IncomeMan.GetComponent<Income>().Spend(HCost);
+		}
+	}
+
+}
