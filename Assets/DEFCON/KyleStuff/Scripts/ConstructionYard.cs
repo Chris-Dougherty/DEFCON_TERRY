f318d46a92ba8a379f3d2807c69e47813bc04a4a
Kmann180 <7319kkennedy@gmail.com>
2015-04-11 22:58:27 -0700

    Spawning Units
    uNIT sPAWNING

---
+++ Assets/DEFCON/KyleStuff/Scripts/ConstructionYard.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConstructionYard : MonoBehaviour 
+{
+
+	// Units building spawns
+	public GameObject Unit;
+	public GameObject AirUnit;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	public void SpawnUnit()
+	{
+		GameObject clone;
+		clone = Instantiate(Unit, new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z - 8), Quaternion.identity) as GameObject;
+	}
+	
+	public void SpawnAirUnit()
+	{
+		GameObject clone;
+		clone = Instantiate(AirUnit, new Vector3(this.transform.position.x, this.transform.position.y + 10, this.transform.position.z - 8), Quaternion.identity) as GameObject;
+	}
+}
